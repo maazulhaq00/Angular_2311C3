6a165bffdae920827e0c105ab946a885a79e7717
Maaz Ul Haq <maazulhaq00@gmail.com>
2025-03-29 11:28:31 +0500

    navigate to prod list on prod create success

--- angular_laravel_crud/frontend/src/app/components/create-product/create-product.component.ts
+++ angular_laravel_crud/frontend/src/app/components/create-product/create-product.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IProduct } from '../../interfaces/product';
 import { HttpService } from '../../http.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-product',
@@ -12,6 +13,7 @@ import { HttpService } from '../../http.service';
 export class CreateProductComponent {
 
   httpService = inject(HttpService);
+  router = inject(Router);
 
   product : IProduct = {
     id : 0,
@@ -26,8 +28,10 @@ export class CreateProductComponent {
     console.log(this.product);
 
     this.httpService.createProduct(this.product).subscribe(()=>{
+      
       console.log("Product Added");
       
+      this.router.navigateByUrl("/all-prod");
     })
     
   }
