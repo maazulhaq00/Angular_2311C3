c5511582ea7308904932172c73d21820b3ae9afd
Maaz Ul Haq <maazulhaq00@gmail.com>
2025-03-29 11:23:26 +0500

    Routing

--- angular_laravel_crud/frontend/src/app/app.routes.ts
+++ angular_laravel_crud/frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { DisplayProductsComponent } from './components/display-products/display-products.component';
+import { CreateProductComponent } from './components/create-product/create-product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: DisplayProductsComponent
+    }, 
+    {
+        path: "all-prod",
+        component: DisplayProductsComponent
+    },
+    {
+        path: "add-prod",
+        component: CreateProductComponent
+    },
+];
